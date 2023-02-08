0cd3bfc52caa2a500b18da84069d95e0f0ec3328
Paul Butler <paul.butler@ni.com>
2023-02-08 09:45:02 -0600

    video index

--- music/scripts/VideoIndex.js
+++ music/scripts/VideoIndex.js
@@ -1,16 +1,15 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=16m00s">16:00</a></td> <td>Gary and Paulette</td> <td>Dream Baby</td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=21m54s">21:54</a></td> <td>Barbara</td> <td><a href=https://ozbcoz.com/Songs/song.php?ID=2056,soprano>Happy Talk</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=26m20s">26:20</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202021/Drift%20Away.pdf>Drift Away</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=32m45s">32:45</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202022/Workin\'%20in%20a%20Coal%20Mine.pdf>Working in a Coal Mine</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=38m24s">38:24</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Scots%20Wha%20Hae.pdf>Scots Wha Hae</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=43m12s">43:12</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Fall%202022/Good%20Riddance.pdf>Good Riddance</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=51m03s">51:03</a></td> <td>Debbie</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/So%20Long%20Marianne.pdf>So Long Marianne</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=58m44s">58:44</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Take%20It%20Easy.pdf>Take it Easy</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=1h04m37s">1:04:37</a></td> <td>Marty</td> <td><a href=https://tuesdayukes.org/music/PDFs/Kevin\'s%20Wake%20Songs/Shady%20Grove.pdf>Shady Grove</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=1h23m40s">1:23:40</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Bye%20Bye%20Blues.pdf>Bye Bye Blues</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=1h31m27s">1:31:27</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202021-22/Ca\'%20the%20Yowes.pdf>Ca\' the Yowes</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=1h41m22s">1:41:22</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/Bridge%20Over%20Troubled%20Water%20-%20A.pdf>Bridge Over Troubled Water</a></td></tr>\
-<tr><td><a href="https://youtu.be/wzvZLL0WGEk?t=1h50m35s">1:50:35</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/ChordPro/don\'t%20dallas%20my%20austin.chopro>Don\'t Dallas My Austin</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=14m40s">14:40</a></td> <td>Gary and Paulette</td> <td>Buffalo Gals</td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=18m19s">18:19</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Go%20Your%20Own%20Way.pdf>Go Your Own Way</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=25m01s">25:01</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202021-22/If%20I%20Was%20a%20Blackbird.pdf>If I Was a Blackbird</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=32m06s">32:06</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Fall%202022/When%20the%20red%20red%20robin.pdf>When the Red, Red Robin</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=37m16s">37:16</a></td> <td>Christine</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Talkin\'%20in%20my%20Sleep.pdf>Talkin\' in my Sleep</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=43m17s">43:17</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Leaving%20on%20a%20Jet%20Plane.pdf>Leavin\' on a Jet Plane</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=48m44s">48:44</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Who\'ll%20Stop%20The%20Rain.pdf>Who\'ll Stop the Rain</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=54m28s">54:28</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/If%20I%20Had%20a%20Hammer.pdf>If I Had a Hammer</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=1h04m56s">1:04:56</a></td> <td>Deana</td> <td>interim music</td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=1h12m38s">1:12:38</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Question.pdf>Question</a></td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=1h18m45s">1:18:45</a></td> <td>Marty</td> <td>London Homesick Blues</td></tr>\
+<tr><td><a href="https://youtu.be/Mdvz5BXo3GM?t=1h23m30s">1:23:30</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202021-22/Heather%20on%20the%20Moor.pdf>Heather on the Moor</a></td></tr>\
 </table>')

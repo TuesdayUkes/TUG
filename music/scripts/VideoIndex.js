cf5c8991c9e7e92d8fd5030e3de80fc4e4abe6b3
Paul Butler <paul.butler@ni.com>
2023-03-04 17:36:43 -0600

    Index

--- music/scripts/VideoIndex.js
+++ music/scripts/VideoIndex.js
@@ -1,14 +1,18 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=13m58s">13:58</a></td> <td>group</td> <td>Jambalaya</td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=18m28s">18:28</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/500%20Miles.pdf>500 Miles</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=21m40s">21:40</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/50%20Ways%20to%20Leave%20Your%20Lover.pdf>50 Ways to Leave Your Lover</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=27m18s">27:18</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Unchained%20Melody.pdf>Unchained Melody</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=31m46s">31:46</a></td> <td>David</td> <td><a href=https://youtu.be/ADDXwIvLoc4>Being Human</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=38m28s">38:28</a></td> <td>Vany</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/Dont%20Fence%20Me%20In%20C.pdf>Don\'t Fence Me In</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=42m09s">42:09</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/House%20of%20the%20Rising%20Sun.pdf>House of the Rising Sun</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=51m37s">51:37</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202021/Fields%20of%20Gold.pdf>Fields of Gold</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=1h01m01s">1:01:01</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/A%20Fool%20Such%20as%20I.pdf>A Fool Such as I</a></td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=1h05m11s">1:05:11</a></td> <td>Paul</td> <td>Just the Two of Us</td></tr>\
-<tr><td><a href="https://youtu.be/U9IVb69kQVs?t=1h16m50s">1:16:50</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=7m50s">7:50</a></td> <td>group</td> <td>Buffalo Gals</td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=13m03s">13:03</a></td> <td>Kent</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/The%20Beehive%20State.pdf>The Beehive State</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=18m15s">18:15</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Don\'t%20You%20Just%20Know%20It.pdf>Don\'t You Just Know It</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=23m20s">23:20</a></td> <td>Marty</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Mama\'s%20Apron%20Strings.pdf>Mama\'s Apron Strings</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=27m10s">27:10</a></td> <td>Vany</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Rocket%20Man.pdf>Rocket Man</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=31m07s">31:07</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Landslide.pdf>Landslide</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=35m45s">35:45</a></td> <td>Christine</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Dream%20Lover.pdf>Dream Lover</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=39m12s">39:12</a></td> <td>Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/After%20Hours.pdf>After Hours</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=42m11s">42:11</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/I\'ll%20Always%20Remember%20Us%20This%20Way.pdf>I\'ll Always Remember Us This Way</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=47m15s">47:15</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Upside%20of%20Lonely.pdf>Upside of Lonely</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=51m55s">51:55</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Ob-La-Di,%20Ob-La-Da.pdf>Ob-La-Di, Ob-La-Da</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=56m18s">56:18</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/She%20Thinks%20I%20Still%20Care.pdf>She Thinks I Still Care</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=1h00m07s">1:00:07</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Charlie\'s%20Ring.pdf>Charlie\'s Ring</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=1h05m50s">1:05:50</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Flowers.pdf>Flowers</a></td></tr>\
+<tr><td><a href="https://youtu.be/YjfAHXCgstI?t=1h16m50s">1:16:50</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
 </table>')

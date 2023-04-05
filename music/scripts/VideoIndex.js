04642b5d8db973b519775831c37c092fd44dc03f
Paul Butler <paul.butler@ni.com>
2023-04-05 11:16:20 -0500

    video index update

--- music/scripts/VideoIndex.js
+++ music/scripts/VideoIndex.js
@@ -1,14 +1,17 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=16m32s">16:32</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202022/El%20Camino.pdf>El Camino</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=20m24s">20:24</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Popeye%20the%20Sailor%20Man.pdf>Popeye the Sailor Man</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=28m26s">28:26</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/The%20Unicorn.pdf>The Unicorn</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=35m23s">35:23</a></td> <td>Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Somewhere%20Only%20We%20Know.pdf>Somewhere Only We Know</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=41m52s">41:52</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Eight%20Days%20a%20Week.pdf>Eight Days a Week</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=53m54s">53:54</a></td> <td>Vany</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Crowded%20Table.pdf>Crowded Table</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=1h00m14s">1:00:14</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Heartaches%20by%20the%20Number.pdf>Heartaches by the Number</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=1h04m14s">1:04:14</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Saint%20Teresa.pdf>Saint Teresa</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=1h10m08s">1:10:08</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Rolling%20Hills%20of%20the%20Borders.pdf>Rolling Hills of the Borders</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=1h14m09s">1:14:09</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Hurt.pdf>Hurt</a></td></tr>\
-<tr><td><a href="https://youtu.be/eGMyIv6xXDc?t=1h18m27s">1:18:27</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=21m55s">21:55</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202022/El%20Camino.pdf>El Camino</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=26m10s">26:10</a></td> <td>group</td> <td>Deep in the Heart of Texas</td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=32m35s">32:35</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Rose%20of%20Cimarron.pdf>Rose of Cimarron</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=40m10s">40:10</a></td> <td>Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/You%20Ain\'t%20Goin\'%20Nowhere.pdf>You Ain\'t Goin\' Nowhere</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=43m26s">43:26</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Fall%202020/After%20the%20Gold%20Rush.pdf>After the Gold Rush</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=49m28s">49:28</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Big%20Bad%20Bill.pdf>Big Bad Bill</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=52m50s">52:50</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Bully%20of%20the%20Town.pdf>Bully of the Town</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=59m55s">59:55</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Last%20Kind%20Words.pdf>Last Kind Words</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h04m50s">1:04:50</a></td> <td>Mike</td> <td>Time in a Bottle</td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h11m30s">1:11:30</a></td> <td>Vany</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Baby%20Can%20I%20Hold%20You.pdf>Baby Can I Hold You</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h17m02s">1:17:02</a></td> <td>Marty</td> <td>Red Haired Boy</td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h27m05s">1:27:05</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Fall%202022/The%20Battle%20of%20New%20Orleans.pdf>The Battle of New Orleans</a></td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h31m04s">1:31:04</a></td> <td>Christine</td> <td>A Song for You</td></tr>\
+<tr><td><a href="https://youtu.be/VYJGe2Pjblk?t=1h35m35s">1:35:35</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
 </table>')

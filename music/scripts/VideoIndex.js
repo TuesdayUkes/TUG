8c4f455c41b3558c28cfd3ce9bfcadb36c97d2ac
Paul Butler <paul.butler@ni.com>
2023-05-03 15:51:57 -0500

    Video Index

--- music/scripts/VideoIndex.js
+++ music/scripts/VideoIndex.js
@@ -1,14 +1,14 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=6m48s">6:48</a></td> <td>group</td> <td>Achy Breaky Heart</td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=11m25s">11:25</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202021-22/Jamaica%20Farewell.pdf>Jamaica Farewell</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=15m51s">15:51</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Chocolate%20Jesus.pdf>Choc-o-late Jesus</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=28m33s">28:33</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Carolina%20in%20the%20Pines.pdf>Carolina in the Pines</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=35m15s">35:15</a></td> <td>Vany</td> <td><a href=https://tuesdayukes.org/music/PDFs/Fall%202022/People%20Get%20Ready.pdf>People Get Ready</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=40m32s">40:32</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Winter%202022-23/Flowers.pdf>Flowers</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=47m05s">47:05</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/City%20Of%20Stars.pdf>City of Stars</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=53m03s">53:03</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/I%20Remember%20Everything.pdf>I Remember Everything</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=1h00m05s">1:00:05</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202021/Hooked%20on%20a%20Feeling.pdf>Hooked on a Feeling</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=1h06m06s">1:06:06</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/London%20Homesick%20Blues.pdf>London Homesick Blues</a></td></tr>\
-<tr><td><a href="https://youtu.be/h-48_ALWlF4?t=1h12m06s">1:12:06</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=12m06s">12:06</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Coney%20Island%20Washboard%20Roundelay.pdf>Coney Island Washboard</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=15m10s">15:10</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/El%20Paso.pdf>El Paso</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=24m45s">24:45</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Because%20the%20Night.pdf>Because the Night</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=30m06s">30:06</a></td> <td>Marty</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Dream%20of%20a%20Miner\'s%20Child.pdf>Dream of a Miner\'s Child</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=36m10s">36:10</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/As%20Tears%20Go%20By.pdf>As Tears Go By</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=43m16s">43:16</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Blue%20Bayou.pdf>Blue Bayou</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=47m23s">47:23</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/If%20You%20Could%20Read%20My%20Mind.pdf>If You Could Read My Mind</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=54m50s">54:50</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Let%20the%20Mystery%20Be.pdf>Let the Mystery Be</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h01m09s">1:01:09</a></td> <td>Mark</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/The%20Gambler.pdf>The Gambler</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h07m14s">1:07:14</a></td> <td>Mark</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/Folsom%20Prison%20Blues-DP.pdf>Folsom Prison Blues</a></td></tr>\
+<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h12m06s">1:12:06</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
 </table>')

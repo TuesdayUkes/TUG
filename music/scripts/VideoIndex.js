4084ebc7012f43f091010db34957efc4e01c729c
Paul Butler <paul.butler@ni.com>
2023-05-10 10:33:38 -0500

    video index update

--- music/scripts/VideoIndex.js
+++ music/scripts/VideoIndex.js
@@ -1,14 +1,11 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=12m06s">12:06</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Coney%20Island%20Washboard%20Roundelay.pdf>Coney Island Washboard</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=15m10s">15:10</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/El%20Paso.pdf>El Paso</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=24m45s">24:45</a></td> <td>Paulette and Gary</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Because%20the%20Night.pdf>Because the Night</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=30m06s">30:06</a></td> <td>Marty</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Dream%20of%20a%20Miner\'s%20Child.pdf>Dream of a Miner\'s Child</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=36m10s">36:10</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/As%20Tears%20Go%20By.pdf>As Tears Go By</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=43m16s">43:16</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Blue%20Bayou.pdf>Blue Bayou</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=47m23s">47:23</a></td> <td>Paul</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/If%20You%20Could%20Read%20My%20Mind.pdf>If You Could Read My Mind</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=54m50s">54:50</a></td> <td>Barbara</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Let%20the%20Mystery%20Be.pdf>Let the Mystery Be</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h01m09s">1:01:09</a></td> <td>Mark</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/The%20Gambler.pdf>The Gambler</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h07m14s">1:07:14</a></td> <td>Mark</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/Folsom%20Prison%20Blues-DP.pdf>Folsom Prison Blues</a></td></tr>\
-<tr><td><a href="https://youtu.be/Kg2i7fZTEXQ?t=1h12m06s">1:12:06</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=20m29s">20:29</a></td> <td>Paul</td> <td><a href=https://youtu.be/OsAMEyCStxQ>Are You My Sunshine?</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=23m49s">23:49</a></td> <td>Brenda</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Looking%20Out%20My%20Back%20Door.pdf>Looking Out My Back Door</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=26m31s">26:31</a></td> <td>Walter</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202021/If%20You%20Want%20to%20be%20a%20Bird.pdf>If You Want to be a Bird</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=30m42s">30:42</a></td> <td>Mary Jane</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/Wagon%20Wheels.pdf>Wagon Wheels</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=40m00s">40:00</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/The%20Gambler.pdf>The Gambler</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=44m56s">44:56</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202020/Folsom%20Prison%20Blues-DP.pdf>Folsom Prison Blues</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=51m08s">51:08</a></td> <td>Gary and Paulette</td> <td><a href=https://tuesdayukes.org/music/PDFs/Spring%202023/The%20Watchman\'s%20Gone.pdf>The Watchman\'s Gone</a></td></tr>\
+<tr><td><a href="https://youtu.be/HPKV_AKgsas?t=56m30s">56:30</a></td> <td>group</td> <td><a href=https://tuesdayukes.org/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></tr>\
 </table>')

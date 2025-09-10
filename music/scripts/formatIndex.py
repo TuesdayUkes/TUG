1fc548bcc240bef020dba1d41e296521ba2cfc02
Paul Butler <c.paul.butler@gmail.com>
2025-09-09 23:18:27 -0500

    video index

--- music/scripts/formatIndex.py
+++ music/scripts/formatIndex.py
@@ -26,20 +26,20 @@ with open(HtmlFilename, "w") as HtmlFile:
     newL = l
 
     # Add <td></td> around player's name
-    newL = re.sub("(\d+ +)(.*)( \()", r"\1<td>\2</td>\3", newL)
+    newL = re.sub(r"(\d+ +)(.*)( \()", r"\1<td>\2</td>\3", newL)
 
     # For timestamps greater than an hour, look for three numbers separated by 2 colons
-    newL = re.sub("^(\d+):(\d+):(\d+)", f"<td><a href=\"{youtubeLink}?t=\\1h\\2m\\3s\">\\1:\\2:\\3</a></td>",newL)
+    newL = re.sub(r"^(\d+):(\d+):(\d+)", f"<td><a href=\"{youtubeLink}?t=\\1h\\2m\\3s\">\\1:\\2:\\3</a></td>",newL)
 
     # For timestamps less than an hour, there are only two time fields (minutes : seconds)
-    newL = re.sub("^(\d+):(\d+)", f"<td><a href=\"{youtubeLink}?t=\\1m\\2s\">\\1:\\2</a></td>",newL)
+    newL = re.sub(r"^(\d+):(\d+)", f"<td><a href=\"{youtubeLink}?t=\\1m\\2s\">\\1:\\2</a></td>",newL)
 
     # reshape URL for sheet music into an HTML tag using the song title as the
     # visible text
-    newL = re.sub("\(([^)]*)\) (https?:.*)", r'<td><a href="\2">\1</a></td>',newL)
+    newL = re.sub(r"\(([^)]*)\) (https?:.*)", r'<td><a href="\2">\1</a></td>',newL)
 
     # If song title wasn't used with a link, split it from the player name here:
-    newL = re.sub("\((.*)\)$", r"<td>\1</td>", newL)
+    newL = re.sub(r"\((.*)\)$", r"<td>\1</td>", newL)
 
     HtmlFile.write(newL.rstrip())
 

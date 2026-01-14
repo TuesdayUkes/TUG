4398d1596cdd55ab9709ac899ae515bf30aaa8af
Paul Butler <c.paul.butler@gmail.com>
2026-01-14 13:43:11 -0600

    force execution in conhost and set console size to fit small screen

--- GitPull.bat
+++ GitPull.bat
@@ -1,4 +1,10 @@
 @echo off
+if not "%CONHOST_LAUNCHED%"=="1" (
+    set CONHOST_LAUNCHED=1
+    start "GitPull" conhost.exe cmd.exe /c "%~f0"
+    exit
+)
+mode con: cols=50 lines=30
 cd c:\users\PC\TUG
 if errorlevel 1 goto error
 

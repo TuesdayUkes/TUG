a73423d980334a9c1958564df07b059daf285e22
Paul Butler <c.paul.butler@gmail.com>
2026-03-11 14:21:50 -0500

    add group photo with Vany

--- includes/header.php
+++ includes/header.php
@@ -18,4 +18,4 @@
     <link rel="stylesheet" href="/wp-content/plugins/the-events-calendar/common/src/resources/css/tooltip.min.css@ver=4.12.10.css">
     
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="/styles/main.css?v=2026.02.28.14.15.55">
+    <link rel="stylesheet" href="/styles/main.css?v=2026.03.11.14.21.50">
